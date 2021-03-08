c9e3bc783ee6fca01ec543e82f87e7ebc578127f
ashification <aisling_brucy@hotmail.com>
2021-03-08 19:15:02 +0000

    Added Reading in System time and writing it to RTC registers

--- DS3231.cpp
+++ DS3231.cpp
@@ -9,14 +9,11 @@
 #include<sys/ioctl.h>
 #include<linux/i2c.h>
 #include<linux/i2c-dev.h>
+#include <time.h>
+ 
 using namespace std;
 
 #define BUFFER_SIZE 19      //0x00 to 0x12
-
-#define RED 6
-#define ORANGE 13
-#define YELLOW 19
-#define BLUE 26
 
 int file;
 
@@ -25,21 +22,6 @@ int bcdToDec(char b) { return (b/16)*10 + (b%16); }
 
 //Converts Decimal value to BCD taken from http://danceswithferrets.org/geekblog/?p=504
 char decTobcd(int d) { return ((d/10) << 4 | (d%10));}
-
-
-// Write a single byte value to a single register.
-int writeRegister(unsigned int registerAddress, unsigned char value){
-   unsigned char buf[2];
-   buf[0] = registerAddress;
-   buf[1] = value;
-   if(write(file, buf, 2)!=2){
-      perror("I2C: Failed write to the device\n");
-      return 1;
-   }
-
-   return 0;
-}
-
 
 int main(){
     	
@@ -68,7 +50,7 @@ int main(){
       return 1;
    }
    
-   printf(" Read RTC Current Values\n");   
+   printf(" Read RTC Current Unset Values\n");   
    printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]), bcdToDec(buf[1]), bcdToDec(buf[0]));
    printf("The RTC Date is %02d-%02d-%02d\n", bcdToDec(buf[4]), bcdToDec(buf[5]), bcdToDec(buf[6]));
    float temperature = buf[0x11] + ((buf[0x12]>>6)*0.25);
@@ -78,10 +60,34 @@ int main(){
    //printf("Buffer 0 = %d, Converted val = %d, Converted back = %d\n", buf[0], bcdToDec(buf[0]), decTobcd(bcdToDec(buf[0])));
 
    // Test Write to Registers
-   printf("Buffer 0 = %d, Converted val = %d\n", buf[1], bcdToDec(buf[1]));
-   buf[0x01] = decTobcd(48);
-   printf("Buffer 0 = %d, Converted val = %d\n", buf[1], bcdToDec(buf[1]));
-   printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]), bcdToDec(buf[1]), bcdToDec(buf[0]));
+   //printf("Buffer 0 = %d, Converted val = %d\n", buf[1], bcdToDec(buf[1]));
+   //buf[0x01] = decTobcd(48);
+   //printf("Buffer 0 = %d, Converted val = %d\n", buf[1], bcdToDec(buf[1]));
+   //printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]), bcdToDec(buf[1]), bcdToDec(buf[0]));
+   
+   //Modified from https://www.includehelp.com/c-programs/system-date-time-linux.aspx
+
+    time_t T= time(NULL);
+    struct  tm tm = *localtime(&T);
+    
+    //printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]), bcdToDec(buf[1]), bcdToDec(buf[0]));
+    //printf("The RTC Date is %02d-%02d-%02d\n", bcdToDec(buf[4]), bcdToDec(buf[5]), bcdToDec(buf[6]));
+	
+    printf("System Date is: %02d/%02d/%02d\n",tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+    printf("System Time is: %02d:%02d:%02d\n\n",tm.tm_hour, tm.tm_min, tm.tm_sec);
+    	
+	buf[0x02] = decTobcd(tm.tm_hour);
+	buf[0x01] = decTobcd(tm.tm_min);
+	buf[0x00] = decTobcd(tm.tm_sec);
+	
+	buf[0x04] = decTobcd(tm.tm_mday);
+	buf[0x05] = decTobcd(tm.tm_mon+1);
+	buf[0x06] = decTobcd(tm.tm_year-100);
+	
+    printf("Set System time to RTC\n");	
+    printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]), bcdToDec(buf[1]), bcdToDec(buf[0]));
+    printf("The RTC Date is %02d-%02d-%02d\n", bcdToDec(buf[4]), bcdToDec(buf[5]), bcdToDec(buf[6]));
+
 
    close(file);
    printf("\n\nScript Complete\n");  
